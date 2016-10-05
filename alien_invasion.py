f2d8795b86b8691478dd824e14578acd8f975ded
Mr Russell <drussell@heyfordparkfreeschool.org>
2016-10-05 07:56:54 +0100

    263264

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,16 +23,7 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-
-        # Get rid of bullets that have disappeared
-
-        for bullet in bullets.copy():
-            if bullet.rect.bottom <= 0:
-               bullets.remove(bullet)
-        # check bullets are being removed
-        print(len(bullets))
-        
+        gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, bullets)
 
         # Make the most recently drawn screen visible
--- game_functions.py
+++ game_functions.py
@@ -24,6 +24,25 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
 
+def update_bullets(bullets):
+    """ Update position of bullets and get rid of old bullets."""
+    # Update bullet positions
+    bullets.update()
+
+    # Get rid of bullets that have disappeared
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """ Fire a bullet if limit not reached yet """
+    # Create a new bullet and add it to the bullets group
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """ respond to keypresses """
     if event.key == pygame.K_RIGHT:
@@ -31,10 +50,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
