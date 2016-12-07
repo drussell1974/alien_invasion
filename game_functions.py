a950201703d158060739f90f7b57a1f61772b0a7
Mr Russell <drussell@heyfordparkfreeschool.org>
2016-12-07 08:56:20 +0000

    283285

--- game_functions.py
+++ game_functions.py
@@ -85,11 +85,15 @@ def change_fleet_direction(ai_settings, aliens):
     ai_settings.fleet_direction *= -1
 
 
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, ship, aliens):
     """ Check if the fleet is at an edge, and then update the positions of all aliens in the fleet """
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
+    # Look for alien-ship collisions
+    if pygame.sprite.spritecollideany(ship, aliens):
+        print("Ship hit!!!")
+        
 
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """ Update position of bullets and get rid of old bullets."""
@@ -101,14 +105,19 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-    # Check for any bullets that have hit aliens.
-    # If so, get rid of the bullet and the alien.
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """ Respond to bullet-alien collisions."""
+    # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
         # Destroy existing bullets and create new fleet
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
+
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """ Fire a bullet if limit not reached yet """
@@ -128,6 +137,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         fire_bullet(ai_settings, screen, ship, bullets)
     elif event.key == pygame.K_q:
         sys.exit()
+
 
 def check_keyup_events(event, ship):
     """ respond to keypresses """
